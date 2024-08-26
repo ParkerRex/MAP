0490ffad27b4e23c88ef7aedc0387b4b7a6ec6f4
Parker Rex <parker.m.rex@gmail.com>
2024-08-26 14:08:06 -0400

    x

--- apps/express/index.ts
+++ apps/express/index.ts
@@ -487,5 +487,5 @@ app.use("/api/", apiLimiter);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is now running on port ${PORT}`);
 });
